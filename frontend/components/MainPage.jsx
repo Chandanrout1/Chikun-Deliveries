e491befdf97636c4974ec2738750ac049177381a
Pravu chandan Rout <140379711+Chandanrout1@users.noreply.github.com>
2024-07-08 23:59:33 +0530

    first commit basic non-functional frontend

---
+++ frontend/components/MainPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Stores from "./Stores";
+import Category from "./Category";
+
+const MainPage = () => {
+  return (
+    <>
+      <Category/>
+      <Stores/>
+      
+    </>
+  );
+};
+
+export default MainPage;
